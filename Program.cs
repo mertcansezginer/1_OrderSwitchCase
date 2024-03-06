98be92c579aaa402efe82249c0066a6e88fa7d97
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 14:10:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Lutfen siparis numaranizi giriniz: ");
+int siparisNumarasi = Convert.ToInt32(Console.ReadLine());
+
+switch (siparisNumarasi)
+{
+    case 100:
+        Console.WriteLine("Siparis edilen urun : Ayran 1L");
+        break;
+    case 101:
+        Console.WriteLine("Siparis edilen urun : Ezine Peyniri 1KG");
+        break;
+    case 102:
+        Console.WriteLine("Siparis edilen urun : Kasar Peyniri 1KG");
+        break;
+    case 103:
+        Console.WriteLine("Siparis edilen urun : Gemlik Zeytin 0.5KG");
+        break;
+    default:
+        Console.WriteLine("Gecersiz bir siparis numarasi girdiniz");
+        break;
+
+
+}
+ 
+
+
